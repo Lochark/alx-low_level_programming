bee66b3479eaa3d0aa35bbedba72ff03a0c037b5
Lochark <charleskarimi@gmail.com>
2023-09-17 18:04:40 -0700

    commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,9 +4,9 @@
  * add - adds two integers and returns the result
  *
  * @j: int to be added to k
- * 
+ *
  * @k: int to be added to j
- * 
+ *
  * Return: sum of j and k
  */
 int add(int j, int k)
