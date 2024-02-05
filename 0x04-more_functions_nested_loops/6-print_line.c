999dbc8e40725aaa89f8de7d3b30f3da9cabd63c
Lochark <charleskarimi@gmail.com>
2024-02-05 06:54:51 -0800

    new files

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,21 +5,19 @@
  *
  * only use _putchar
  *
- * @n: input integer
- *
  * return- result (0) success
  */
 void print_line(int n)
 {
-	int i;
+	int n;
 
-	for (i = 0; i <= n; i++)
+	for (n = 0; n <= 10; n++) 
 	{
-		if (n > 0)
+		if (n <= 0)
 		{
-			_putchar ('_');
+			_putchar ("\n");
 		}
-
+		_putchar (n);
 	}
-	_putchar ('\n');
+	_putchar ("\n");
 }
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *print_diagonal- prints a diagonal line
+ *
+ *@n: repeat integer
+ *
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n > 1)
+		{
+			for (j = 0; j < i; j++)
+			{
+			_putchar (' ');
+			}
+		_putchar ('\\');
+		_putchar ('\n');
+	}
+		if (n <= 0)
+		{
+			_putchar ('\n');
+		}
+	}
+}
